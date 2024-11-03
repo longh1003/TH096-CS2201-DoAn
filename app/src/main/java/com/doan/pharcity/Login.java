a508a208b76d4e639416fd533272e14b9c17a820
dinhluangifhit <dinhluan020202@gmail.com>
2024-11-03 11:07:03 +0700

    Them moi

--- app/src/main/java/com/doan/pharcity/Login.java
+++ app/src/main/java/com/doan/pharcity/Login.java
@@ -6,15 +6,28 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class Login extends AppCompatActivity {
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
 
     private boolean passwordShowing = false;
     @Override
@@ -32,6 +45,26 @@ public class Login extends AppCompatActivity {
         final ImageView PasswordIcon = findViewById(R.id.passwordIcon);
         final TextView ForgotPassword = findViewById(R.id.forgotPassword);
         final TextView SignnUpBnt = findViewById(R.id.signnUpBnt);
+        final RelativeLayout SigiInBntWithGoogle = findViewById(R.id.sigiInBntWithGoogle);
+
+        //Login Google
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        SigiInBntWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+
+        //intern forgotPassword
+        ForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {startActivity(new Intent(Login.this, forgotPassword.class));}
+        });
 
 
         PasswordIcon.setOnClickListener(new View.OnClickListener() {
@@ -62,5 +95,28 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this, Register.class));
             }
         });
+    }
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+            } catch (ApiException e){
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void navigateToSecondActivity(){
+        Intent intent = new Intent(Login.this , SecondActivity.class);
     }
 }
